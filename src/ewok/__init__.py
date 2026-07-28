c01f71b5fa0195f55db7b73fdfd6db2f03e1b878
Robin van der Noord <robinvandernoord@gmail.com>
2026-07-28 12:46:54 +0200

    chore(exports): sort public API names

--- src/ewok/__init__.py
+++ src/ewok/__init__.py
@@ -3,13 +3,13 @@ from .core import Context, Task, find_namespace, namespaces, task, tasks
 from .monkey import format_frame, monkeypatch_invoke
 
 __all__ = [
-    "Task",
-    "task",
     "App",
     "Context",
+    "Task",
     "find_namespace",
     "format_frame",
     "monkeypatch_invoke",
     "namespaces",
+    "task",
     "tasks",
 ]
